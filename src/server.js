49b64a61aaad30ecec180ffb4ea3b72dd6bfa081
dayjang <dianna.jang@gmail.com>
2021-07-21 13:49:51 -0400

    Updated sever.js ~#3.5 Middlewares

--- src/server.js
+++ src/server.js
@@ -1,13 +1,18 @@
 import express from "express";
 const PORT = 4000;            
-const app = express(); // 
-
+const app = express();  
+const gossipMiddleware = (req,res,next) => {
+    console.log(`I'm in the middle! Someone is going to: ${req.url}`);
+    next(); // this gossipMid function call next(), and gossipnMid function become a middleware!!
+}
 const handleHome = (req,res) => {
-    return res.send("I still love You@");
+    return res.send("handle home is executed!");
 };
-
+// we have two handlers and one of them is middleware
+// middleware == handler -- controller... has one more argument 'next'
 // app.get("/",()=>console.log("sb is trying to go home")); // we are receiving a request. but not responding haha
-app.get("/",handleHome);
+
+app.get("/",gossipMiddleware,handleHome);
 
 const handleListening = () => console.log(`server listeing thru http://localhost:${PORT}`);
 app.listen(4000, handleListening); // open server to outside of the world. 

16079d8f5015187d60c991f84ebd324545c4642c
dayjang <dianna.jang@gmail.com>
2021-09-22 18:11:41 -0400

    ~5.7 Conditional2

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,12 @@
 // export const trending = (req,res) => res.send(`<!DOCTYPE html><html lang="en"><head><title>WETUBE!</title></head><body><h1>HOME Trending..</h1></body><footer>ha</footer></html>`);
-export const trending = (req,res)=> res.render("home", { pageTitle: "Home" , potato: "Tomato"});
+const fakeUser = {
+    username: "Diane",
+    loggedIn: true
+    
+};
+
+
+export const trending = (req,res)=> res.render("home", { pageTitle: "Home" , potato: "Tomato", fakeUser: fakeUser});
 export const see = (req,res) => res.render("watch");
 export const edit = (req,res) => res.render("edit");
 

d5bf445ad8f5f54f610e9a523a7f6764ede9e0c0
dayjang <dianna.jang@gmail.com>
2021-09-22 18:24:11 -0400

    ~#5.8 Iteration

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -5,8 +5,8 @@ const fakeUser = {
     
 };
 
-
-export const trending = (req,res)=> res.render("home", { pageTitle: "Home" , potato: "Tomato", fakeUser: fakeUser});
+const videos = [1,2,3,4,5,6,7];
+export const trending = (req,res)=> res.render("home", { pageTitle: "Home" , potato: "Tomato", fakeUser: fakeUser, videos});
 export const see = (req,res) => res.render("watch");
 export const edit = (req,res) => res.render("edit");
 
